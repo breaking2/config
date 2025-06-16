3b18a821b705d0a6a0a205c415d88cef79ac5e54
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-06-16 20:17:43 +0900

    fix: fix syntax missing

--- home/config/nix-ld.nix
+++ home/config/nix-ld.nix
@@ -2,5 +2,5 @@
 {
   # 共有ライブラリについて: https://qiita.com/syoshika_/items/435a2183265b23bc73fa
   programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; []];
+  programs.nix-ld.libraries = with pkgs; [ ];
 }
